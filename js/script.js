aaa39282620c6f01dc529f0a28ba983b1229872b
DenisMiso4enko <denrrmisochenko17@gmail.com>
2022-08-19 01:22:25 +0300

    feat: without secion reviews

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 // Ищем нужные элементы бургер и меню
 const burger = document.querySelector('.burger')
 const menu = document.querySelector('.nav-menu')
+const body = document.body
 // Вещаем событие на бургер и добовляем переключатель с классом active
 burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     menu.classList.toggle('active')
+    body.classList.toggle('stopscroll')
 })
 // При нажатии на сылку из меню удаляем класс active
 document.querySelectorAll('.nav-menu').forEach(n => n.addEventListener('click', () => {
     burger.classList.remove('active')
     menu.classList.remove('active')
+    body.classList.remove('stopscroll')
 }))
 
 
 
 
 // slider
-let swiper = new Swiper(".packeges-content", {
-    slidesPerView: 3,
+let swiperPages = new Swiper(".packeges-content", {
+    slidesPerView: 'auto',
     spaceBetween: 35,
     // loop: true,
     centerSlide: 'true',
     fade: 'true',
     grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    //   dynamicBullets: true,
+    // },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -45,3 +48,5 @@ let swiper = new Swiper(".packeges-content", {
         },
     },
   });
+
+  // slider 2
